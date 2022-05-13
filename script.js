823ad60fa18d678ae15046f850a6312fab8310c8
Felipe Souza <systen3@gmail.com>
2022-05-13 17:28:07 -0300

    atualizaçao e finalizacao

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ let lockBoard = false;
 
 function flipCard() {
     if(lockBoard) return;
+    if(this === firstCard) return;
+
     this.classList.add('flip');
     if(!hasFlippedCard) {
         hasFlippedCard = true;
@@ -28,15 +30,31 @@ function checkForMath () {
 
 function disableCards () {
     firstCard.removeEventListener('click', flipCard);
-    segundCard.removeEventListener('click', flipCard)
+    segundCard.removeEventListener('click', flipCard);
+
+    resetBoard();
 }
 
 function unflipCards() {
+    lockBoard = true;
     setTimeout(() => {
         firstCard.classList.remove('flip');
         segundCard.classList.remove('flip');
+        resetBoard();
     }, 1500);
 }
+
+function resetBoard () {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, segundCard] = [null, null];
+}
+
+(function shuffle(){
+    cards.forEach((card) => {
+        let ramdomPosition = Math.floor(Math.random() * 12);
+        card.style.order = ramdomPosition;
+    })
+})();
 
 cards.forEach((card) => {
     card.addEventListener('click', flipCard)
