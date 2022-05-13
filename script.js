15f96a99fd6fea4fcd6f50d75882c2281d52bcc4
Felipe Souza <systen3@gmail.com>
2022-05-12 23:17:33 -0300

    partes finais

--- script.js
+++ script.js
@@ -1,9 +1,44 @@
 const cards = document.querySelectorAll('.card');
+let hasFlippedCard = false;
+let firstCard, segundCard;
+let lockBoard = false;
 
-function flipCard(){
-    this.classList.toggle('flip');
+function flipCard() {
+    if(lockBoard) return;
+    this.classList.add('flip');
+    if(!hasFlippedCard) {
+        hasFlippedCard = true;
+        firstCard = this;
+        return;
+    }
+
+    segundCard = this;
+    hasFlippedCard = false;
+    checkForMath();
+}
+
+function checkForMath () {
+    if(firstCard.dataset.card === segundCard.dataset.card){
+        disableCards();
+        return;
+    }
+
+    unflipCards();
+}
+
+function disableCards () {
+    firstCard.removeEventListener('click', flipCard);
+    segundCard.removeEventListener('click', flipCard)
+}
+
+function unflipCards() {
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        segundCard.classList.remove('flip');
+    }, 1500);
 }
 
 cards.forEach((card) => {
     card.addEventListener('click', flipCard)
 })
+
